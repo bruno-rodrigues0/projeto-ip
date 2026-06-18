9a645b523ad927da1194e497827d2c451c5f59b9
Bruno Rodrigues <brunorodriguesmtv0@gmail.com>
2026-06-18 15:15:47 -0300

    change ARENA_SPRITE for a surface instead of an image

--- src/core/assets.py
+++ src/core/assets.py
@@ -1,11 +1,14 @@
 import pygame
 
+import core.constants as const
+
 # Load sprites (png, webp or jpg for web compatibility)
 ICON = pygame.image.load("src/assets/icon.png")
 MENU_SPRITE = pygame.image.load("src/assets/img/menu_sprite.png")
 HEART_SPRITE = pygame.image.load("src/assets/img/heart2.png")
 COIN_SPRITE = pygame.image.load("src/assets/img/coin.png")
-ARENA_SPRITE = pygame.image.load("src/assets/img/white_bar.png")
+ARENA_SPRITE = pygame.Surface((5, 280))
+ARENA_SPRITE.fill(const.WHITE)
 
 # Load audio (ogg for web compatibility)
 DEBUG_THEME = pygame.mixer.music.load("src/assets/sfx/theme.ogg")
