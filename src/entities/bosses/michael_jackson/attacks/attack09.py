6818ab01f0b3b6f9d731b8d6afc857ba1e23262a
LucasRafhael <lrsa2@cin.ufpe.br>
2026-07-01 17:07:06 -0300

    problemas de testes anteriores normalizado, ataque10

---
+++ src/entities/bosses/michael_jackson/attacks/attack09.py
@@ -0,0 +1,36 @@
+import pygame
+from core import assets
+from entities.enemy_attack import EnemyAttack, Projectile
+
+
+class ProjectileChuva(Projectile):
+
+    def __init__(self, image, x, y):
+        super().__init__(image, x, y)
+
+    def move(self, dt: float) -> None:
+        self.ay += 120 * dt
+
+
+class Attack09(EnemyAttack):
+    attack_time = 4
+    running_time = 0.0
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.running_time = 0.0
+        self.create_projectiles()
+
+
+    def create_projectiles(self) -> None:
+
+        self.projectiles.append(ProjectileChuva(assets.S_AYUWOKI_ATTACK, 500, 150))
+        self.projectiles.append(ProjectileChuva(assets.S_HEE_HEE_ATTACK, 560, 40))
+        self.projectiles.append(ProjectileChuva(assets.S_SMOOTH_ATTACK, 620, -70))
+        self.projectiles.append(ProjectileChuva(assets.S_CRIMINAL_ATTACK, 500, -180))
+
+
+    def update(self, dt: float) -> None:
+        for proj in self.projectiles:
+            proj.move(dt)
+            proj.update(dt)
--- src/entities/bosses/michael_jackson/attacks/attack_list.py
+++ src/entities/bosses/michael_jackson/attacks/attack_list.py
@@ -6,4 +6,4 @@ from entities.bosses.michael_jackson.attacks.attack05 import Attack05
 from entities.bosses.michael_jackson.attacks.attack06 import Attack06
 from entities.bosses.michael_jackson.attacks.attack07 import Attack07
 from entities.bosses.michael_jackson.attacks.attack08 import Attack08
-ATTACK_LIST = [Attack08]
+ATTACK_LIST = [Attack01, Attack02, Attack03, Attack04, Attack05, Attack06, Attack07, Attack08]
