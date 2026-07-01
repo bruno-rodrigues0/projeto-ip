e5841fd3d4d097a9aafa5cff2c2ffc1fc23bc241
Bruno Rodrigues <brunorodriguesmtv0@gmail.com>
2026-07-01 16:14:12 -0300

    bug fix

--- src/components/achievements.py
+++ src/components/achievements.py
@@ -1,4 +1,7 @@
+import json
+
 from components.file_manager import FileManager
+
 
 class AchievementsManager(FileManager):
     file_name = "achievements.json"
@@ -9,3 +12,21 @@ class AchievementsManager(FileManager):
         "no_item": False,
         "shit": False,
     }
+
+    def load_file(self) -> None:
+        try:
+            file_path = self.get_data_path()
+            with open(file_path, "r") as file:
+                data = json.load(file)
+                for key in data:
+                    if key not in list(self.data.keys()):
+                        data.remove(key)
+
+                for key in self.data:
+                    if key not in data.keys():
+                        data[key] = self.data[key]
+
+                self.data = data
+
+        except Exception:
+            self.save_file()
