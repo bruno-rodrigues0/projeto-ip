265e5d9b90649b59bd5d96b73a0792675908d05e
Bruno Rodrigues <brunorodriguesmtv0@gmail.com>
2026-07-01 22:28:39 -0300

    bug fix

--- src/entities/bosses/michael_jackson/instance.py
+++ src/entities/bosses/michael_jackson/instance.py
@@ -1,14 +1,9 @@
 import core.assets as assets
 from components.animation import AnimationPlayer
-from entities.bosses.michael_jackson.attacks.attack01 import Attack01
-from entities.bosses.michael_jackson.attacks.attack02 import Attack02
-from entities.bosses.michael_jackson.attacks.attack03 import Attack03
-from entities.bosses.michael_jackson.attacks.attack04 import Attack04
-from entities.bosses.michael_jackson.attacks.attack05 import Attack05
-from entities.bosses.michael_jackson.attacks.attack06 import Attack06
 from entities.boss import Boss
+from entities.bosses.michael_jackson.attacks.attack_list import ATTACK_LIST
 
-ATTACK_LIST = [Attack01(), Attack02(), Attack03(), Attack04(), Attack05(), Attack06()]
+
 
 BOSS_MICHAEL_JACKSON = Boss(
     "Michael Jackson",
